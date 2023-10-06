e6fd5d6b2e329d8df96f630321fb634571dfd9d4
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2023-10-06 18:17:45 +0200

    Updating

---
+++ .history/src/app/components/generic/button_20231006181712.tsx
@@ -0,0 +1,11 @@
+import { MouseEventHandler } from "react";
+
+export default function Button({ children, className, onClick, disabled=false } : { children: React.ReactNode, className: string, onClick?: undefined | MouseEventHandler<HTMLButtonElement>, disabled?: boolean }){
+    return(
+        <button className={`${disabled ? "bg-slate-500 hover:bg-slate-500 cursor-not-allowed " : "bg-blue-500 hover:bg-opacity-100 cursor-pointer "} ${className} px-5 py-3 rounded-lg bg-opacity-90 active:bg-opacity-80 transition-all flex items-center gap-2 place-items-center`}
+            onClick={onClick}
+            disabled={disabled}>
+            {children}
+        </button>
+    )
+}
---
+++ .history/src/app/integration/globalMethods_20231006181712.tsx
@@ -0,0 +1,9 @@
+export function GetDate(value?: string){
+
+    let date = value ? new Date(value) : new Date();
+    let yyyy = date.getFullYear()
+    let mm = String(date.getMonth() + 1).padStart(2, '0')
+    let dd = String(date.getDate()).padStart(2, '0')
+    
+    return yyyy + '-' + mm + '-' + dd
+}
